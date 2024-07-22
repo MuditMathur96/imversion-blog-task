04fa7fb3a7881697866c9b3b08572dc68bb21f65
Mudit Mathur <mudit.manu.mathur@gmail.com>
2024-07-22 21:30:24 +0530

    small fixes

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8000/"
+export const BACKEND_URL = process.env.BACKEND_URL|| "http://localhost:8000"
 
 export const CATEGORIES=[{
     id:"1",
--- node-graphql/src/infrastructure/database/prisma.ts
+++ node-graphql/src/infrastructure/database/prisma.ts
@@ -1,5 +1,6 @@
 import {PrismaClient} from '@prisma/client';
 
+console.log(process.env.DATABASE_URL);
 const prisma = new PrismaClient();
 
 export default prisma;
